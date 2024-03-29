3aac85db2db96c6ae7ab680e135b126ea413f8c0
Student <mutesi-alin>
2024-03-29 11:05:32 +0300

    added file

---
+++ function.js
@@ -0,0 +1,47 @@
+//1.Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+function sort(){
+    let arr1 = ["mango", "orange", "banana", "apple"];
+    console.log(arr1.sort());
+    console.log(arr1.reverse());
+}
+sort()
+
+//2.Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+let arr =arr.forEach(element => {
+    if (element > 0) {
+      console.log("positive");
+    } else if (element < 0) {
+      console.log("negative");
+    } else {
+      console.log("zero");
+    }
+ });
+
+
+
+
+//3.Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
+
+let customer = [
+    { id: 7 },
+    { name: "aline" },
+    { money: [4000, 2000, 7000, 3000] },
+  ];
+    customer.sort(function(x, y) {
+    return x.money - (y.money);
+  });
+  customer.forEach((person) => console.log(customer.money));
+ console.log(customer);
+
+
+//4.Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
+function multiply(numbers) {
+    numbers.forEach((number) => {
+       console.log(number * 2);
+    });
+   }
+
+   multiply([6, 7, 8, 9]);
+
+//5.Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
+
